4d9587d510a88a608c9e7c2bf3561e8c05342267
Roman Pilip <roman.pilip@actum.cz>
2018-08-01 14:07:22 +0200

    registration page

--- pages/registration.jsx
+++ pages/registration.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from '../components/Modal';
-import RegistrationForm from '../components/RegistrationForm';
+import RegistrationForm from '../components/forms/RegistrationForm';
+import Snackbar from '../components/Snackbar';
 
 import '../styles/main.scss';
 
-export default () => {
-    return (
-        <Modal
-            fullWidth
-            open
-            title="Registration"
-            titleClass="text-center"
-        >
-            <RegistrationForm />
-        </Modal>
-    );
+export default class extends Component {
+    state = {
+        snackbar: {
+            autohideDuration: 5000,
+            open: false,
+            message: 'Message',
+            variant: 'success',
+        }
+    };
+
+    onClose = () => {
+        this.setState((prevState) => {
+            return {
+                snackbar: {
+                    ...prevState.snackbar,
+                    open: false,
+                }
+            }
+        })
+    };
+
+    handleSnackbar = (newSnackbar) => {
+        this.setState({
+            snackbar: newSnackbar
+        });
+    }
+
+    render() {
+        return (
+            <>
+                <RegistrationForm
+                    handleSnackbar={this.handleSnackbar}
+                />
+                <Snackbar 
+                    {...
+                        this.state.snackbar
+                    }
+                    onClose={this.onClose}
+                />
+            </>
+        );
+    }
 };

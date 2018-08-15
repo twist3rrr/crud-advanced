bab25e476f050be3c62d591e5e855bc494af35d8
Roman Pilip <roman.pilip@actum.cz>
2018-08-15 16:12:03 +0200

    main layout for users page

---
+++ pages/users.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/main.scss';
+
+export default () => {
+    return (
+        <div className="layout">
+            <div className="layout__header">HEADER</div>
+            <div className="layout__menu">MENU</div>
+            <div className="layout__main">MAIN</div>
+            <div className="layout__footer">FOOTER</div>
+        </div>
+    );
+};
--- utilities/index.js
+++ utilities/index.js
@@ -1,5 +1,4 @@
 export const defaultStateHandler = (_this) => (newState, callback) => {
-    console.log(_this);
     _this.setState({
         ...newState,
     }, callback);

72980d4ec897de7e60c23c51d0b60a55a7622a5a
Roman Pilip <roman.pilip@actum.cz>
2018-09-01 18:19:40 +0200

    pagination | intermediate

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { defaultStateHandler } from '../utilities';
+import url from 'url';
 import fetch from 'isomorphic-unfetch';
 
 import JSSProvider from '../components/JSSProvider';
@@ -9,11 +10,19 @@ import JSSProvider from '../components/JSSProvider';
 import Header from './../components/pageComponents/Header';
 import Main from './../components/pageComponents/Main';
 import Sidebar from './../components/pageComponents/Sidebar';
+import Spinner from './../components/Spinner';
+
+import { DOMEN } from '../server/constants.js';
 
 import '../styles/main.scss';
 
+const config = {
+    itemsOnPage: 4,
+};
+
 export default class Index extends Component {
     static propTypes = {
+        total: PropTypes.number.isRequired,
         users: PropTypes.array.isRequired,
     };
 
@@ -21,16 +30,16 @@ export default class Index extends Component {
         super(props);
         this.state = {
             amountOfUsers: props.total,
+            itemsOnPage: config.itemsOnPage,
+            isLoading: false,
             currentPage: 1,
-            itemsOnPage: 3,
-            loadedPages: 1,
             users: props.users,
             userName: '',
         };
     }
 
     static async getInitialProps() {
-        const res = await fetch(`http://localhost:3000/getusers?page=${1}&items=${3}`);
+        const res = await fetch(`http://localhost:3000/getusers?page=${1}&items=${config.itemsOnPage}`);
         let users;
         let total;
 
@@ -48,6 +57,7 @@ export default class Index extends Component {
 
     render() {
         const {
+            amountOfPages,
             amountOfUsers,
             currentPage,
             itemsOnPage,
@@ -55,6 +65,19 @@ export default class Index extends Component {
             userName,
             users,
         } = this.state;
+
+        const fetchUrl = url.format({
+            pathname: 'users',
+            protocol: 'http:',
+            host: DOMEN,
+            query: {
+                page: 1,
+                items: config.itemsOnPage,
+                filterName: 'Roman Pylyp',
+            },
+        });
+
+        console.log(fetchUrl);
 
         return (
             <JSSProvider>
@@ -73,6 +96,7 @@ export default class Index extends Component {
                     <div className="layout__main">
                         <Main
                             {...{
+                                amountOfPages,
                                 amountOfUsers,
                                 currentPage,
                                 itemsOnPage,
@@ -83,6 +107,7 @@ export default class Index extends Component {
                         />
                     </div>
                     <div className="layout__footer">FOOTER</div>
+                    { this.state.isLoading && <Spinner /> }
                 </div>
             </JSSProvider>
         );
